40054461f7131fa87f70ead3e7d8b01e023ae331
Nikhil Dev Goyal <98657441+Nikhil0250@users.noreply.github.com>
2024-03-22 17:57:10 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+    # background_image = Image.open("wallpaper3.png")
+    # resized_image = background_image.resize((2048, 1024))
+    # background_photo = ImageTk.PhotoImage(resized_image)
+    # # Create a label with the background image
+    # background_label = tk.Label(amplified_page, image=background_photo)
+    # background_label.image = background_photo  # Store the reference
+    # background_label.pack(expand=True, fill=tk.BOTH)
